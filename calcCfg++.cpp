ffc13a0e24145fea7d8dd663f80c8cd8fb180a51
scalpel <sebastiencapel.sc@gmail.com>
2017-01-03 08:42:55 +0100

    Just a change in calc_cfg.cpp for test

--- calcCfg++.cpp
+++ calcCfg++.cpp
@@ -32,16 +32,23 @@ int main() {
 
 	/*Creation d'un paramètre*/
 	ParamInt val1("val1",inv_on,5,0,10);
+
    ParamInt val2("val2",inv_on,10,0,10);
+
 	ParamInt CONF_FPGA(OUT,&(ond_s_cfg.Brass_cfg.CONF_FPGA),"CONF_FPGA",inv_on,0,0,10);
+
 	val1.printInfo();
 	val2.printInfo();
 	CONF_FPGA.printInfo();
 	CONF_FPGA = val1 + val2;
 	CONF_FPGA.printInfo();
 
-//	LowPassFilter filtre(&ond_s_cfg.beat_cfg.COEF_FLT_PB1_FCAT,"Filtre.nomMMAP",inv_on,gain,ti,te);
-//	filtre.printInfo();
+	ParamFloat gain("gain",inv_on,10.0,0,100);
+	ParamFloat ti("ti",inv_on,10.0e-3,0,100);
+	ParamFloat te("te",inv_on,500.0e-6,0,100);
+
+	LowPassFilter filtre(&ond_s_cfg.beat_cfg.COEF_FLT_PB1_FCAT,"Filtre.nomMMAP",inv_on,gain,ti,te);
+	filtre.printInfo();
 
 	inv_on.printInfo();
 
